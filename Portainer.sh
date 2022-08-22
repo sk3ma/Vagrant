e8772cb60345ac6b5c61173decd573d027cc1843
Levon Eakin <52137438+sk3ma@users.noreply.github.com>
2022-08-22 10:37:30 +0200

    Update Portainer.sh

--- Portainer.sh
+++ Portainer.sh
@@ -21,12 +21,13 @@ install() {
     echo -e "\e[32;1;3mUpdating system\e[m"
     apt update
     echo -e "\e[32;1;3mAdding repository\e[m"
-    apt install apt-transport-https ca-certificates software-properties-common curl -qy
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
+    apt install ca-certificates apt-transport-https software-properties-common curl -qy
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
     echo -e "\e[32;1;3mInstalling Docker\e[m"
-    apt install docker-ce docker-ce-cli containerd.io -qy
-    usermod -aG docker ${USER} && chmod a=rw /var/run/docker.sock
+    apt update && apt install docker-ce docker-ce-cli containerd.io -qy
+    usermod -aG docker ${USER}
+    chmod a=rw /var/run/docker.sock
     echo -e "\e[32;1;3mInstalling Compose\e[m"
     curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
@@ -39,19 +40,19 @@ config() {
     docker pull docker/whalesay:latest
     docker run docker/whalesay:latest cowsay "Docker is functional."
     echo -e "\e[32;1;3mCreating volume\e[m"
-    docker volume create container
+    docker volume create /container
     echo -e "\e[32;1;3mStarting service\e[m"
     systemctl start docker && systemctl enable docker
 }
 
 # Firewall creation.
-#firewall() {
-#    echo -e "\e[32;1;3mAdjusting firewall\e[m"
-#    ufw allow 8000/tcp
-#    ufw allow 9443/tcp
-#    echo "y" | ufw enable
-#    ufw reload
-#}
+firewall() {
+    echo -e "\e[32;1;3mAdjusting firewall\e[m"
+    ufw allow 8000/tcp
+    ufw allow 9443/tcp
+    echo "y" | ufw enable
+    ufw reload
+}
 
 # Portainer server.
 server() {
@@ -60,11 +61,11 @@ server() {
     docker run -d \
     -p 8000:8000 \
     -p 9443:9443 \
-    --name=portainer \
+    --name portainer \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    -v container:/data \
+    -v /container:/data \
     portainer/portainer-ce:latest \
-    --restart=unless-stopped
+    --restart=always
     docker start portainer
 }
 
@@ -76,7 +77,7 @@ agent() {
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v /var/lib/docker/volumes:/var/lib/docker/volumes \
     -v /:/host \
-    -v container:/data \
+    -v /container:/data \
     --restart=always \
     -e EDGE=1 \
     -e EDGE_ID=f28a53ea-2fe3-4ddc-b30a-1687d9f12ae4 \
@@ -94,7 +95,7 @@ if [[ -f /etc/lsb-release ]]; then
     echo -e "\e[35;1;3;5mUbuntu detected, proceeding...\e[m"
     install
     config
-#    firewall
+    firewall
     server
     agent
 fi
