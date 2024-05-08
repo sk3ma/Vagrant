4802128c773aa50641de202d4c7d0ae600079224
Levon Eakin <52137438+sk3ma@users.noreply.github.com>
2024-05-08 14:38:26 +0200

    Updated file

--- Semaphore/Docker.sh
+++ Semaphore/Docker.sh
@@ -25,15 +25,15 @@ install() {
     usermod -aG docker ${USER}
     chmod a=rw /var/run/docker.sock
     echo -e "\e[32;1;3m[INFO] Creating volume\e[m"
-    mkdir -pv /container
+    mkdir -p /container
     docker volume create bindmount
 }
 
 # Enabling service.
 service() {
     echo -e "\e[32;1;3m[INFO] Testing Docker\e[m"
-    docker pull docker/whalesay:latest
-    docker run docker/whalesay:latest cowsay "Docker is functional."
+    docker pull mbentley/cowsay
+    docker run -it --rm mbentley/cowsay "Docker is functional."
     echo -e "\e[32;1;3m[INFO] Starting service\e[m"
     echo "
   ____             _             
@@ -48,8 +48,55 @@ service() {
 
 # Docker Compose.
 compose() {
-    echo -e "\e[32;1;3m[INFO] Executing file up\e[m"
-    docker-compose -f docker-compose.yml up -d
+    echo -e "\e[32;1;3m[INFO] Creating file up\e[m"
+    tee /opt/docker-compose.yml << STOP > /dev/null
+version: "3"
+
+services:
+  mysql:
+    restart: unless-stopped
+    image: mysql:8.0
+    hostname: mysql
+    volumes:
+      - semaphore-mysql:/var/lib/mysql
+    environment:
+      MYSQL_RANDOM_ROOT_PASSWORD: "yes"
+      MYSQL_DATABASE: semaphore
+      MYSQL_USER: semaphore
+      MYSQL_PASSWORD: semaphore
+  semaphore:
+    restart: unless-stopped
+    ports:
+      - 3000:3000
+    image: semaphoreui/semaphore:latest
+    environment:
+      SEMAPHORE_ACCESS_KEY_ENCRYPTION: QfWtJhFYtIcAOFDbbZjpk9hFMobxY0WZf4o1PKnRQKI=
+      SEMAPHORE_ADMIN: osadmin
+      SEMAPHORE_ADMIN_EMAIL: admin@semaphore.local
+      SEMAPHORE_ADMIN_NAME: osadmin
+      SEMAPHORE_ADMIN_PASSWORD: BOPZ3GQ=
+      SEMAPHORE_DB: semaphore
+      SEMAPHORE_DB_DIALECT: mysql
+      SEMAPHORE_DB_HOST: mysql
+      SEMAPHORE_DB_PASS: semaphore
+      SEMAPHORE_DB_PORT: 3306
+      SEMAPHORE_DB_USER: semaphore
+      SEMAPHORE_LDAP_ACTIVATED: "no"
+      SEMAPHORE_LDAP_DN_SEARCH: "uid=bind_user,cn=users,cn=accounts,dc=local,dc=shiftsystems,dc=net"
+      SEMAPHORE_LDAP_HOST: dc01.local.example.com
+      SEMAPHORE_LDAP_NEEDTLS: "yes"
+      SEMAPHORE_LDAP_PASSWORD: "<ldap_bind_password>"
+      SEMAPHORE_LDAP_PORT: "636"
+      SEMAPHORE_LDAP_SEARCH_FILTER: "(\u0026(uid=%s)(memberOf=cn=ipausers,cn=groups,cn=accounts,dc=local,dc=example,dc=com))"
+      SEMAPHORE_PLAYBOOK_PATH: /tmp/semaphore/
+      ANSIBLE_HOST_KEY_CHECKING: "False"
+    depends_on:
+      - mysql
+volumes:
+  semaphore-mysql:
+STOP
+    echo -e "\e[32;1;3m[INFO] Executing file\e[m"
+    docker-compose -f /opt/docker-compose.yml up -d
     echo -e "\e[33;1;3;5m[✅] Finished, Docker installed.\e[m"
     exit
 }
